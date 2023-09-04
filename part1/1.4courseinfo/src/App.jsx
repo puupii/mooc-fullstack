f6938665e0065a7b9c77df240ff98894c81ccfb4
ropo <gitlab@roponkolo.fi>
2023-09-04 10:57:01 +0300

    modified:   1.4courseinfo/src/App.jsx

--- part1/1.4courseinfo/src/App.jsx
+++ part1/1.4courseinfo/src/App.jsx
@@ -1,24 +1,20 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
   const parts = [
-    part1,
-    part2,
-    part3
-  ]
-  
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]  
+
   const Header = (props) => {
     return (
       <div>
@@ -48,9 +44,7 @@ const App = () => {
   const Total = (props) => {
     let totalexercises = 0
     props.parts.forEach(part => {
-      console.log(part.exercises)
       totalexercises += part.exercises;
-      console.log(totalexercises)
     })
     return (
       <div>
