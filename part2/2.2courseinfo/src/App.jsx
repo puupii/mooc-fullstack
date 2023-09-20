39ea6767f5caf63b50b69c8b146442d0ceb45a3b
ropo <gitlab@roponkolo.fi>
2023-09-20 12:54:27 +0300

    2.2courseinfo now shows total number of exercises

---
+++ part2/2.2courseinfo/src/App.jsx
@@ -0,0 +1,75 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'timin terraria linna',
+        exercises: 12,
+        id: 4
+      },
+      {
+        name: 'useless stuuff',
+        exercises: 99,
+        id: 5
+      }
+    ]
+  }
+
+
+  return <Course course={course} />
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header coursename = {course.name} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
+    </div>
+  )
+}
+
+const Header = ({coursename}) => {
+  return(
+    <div>
+    <h1> {coursename} </h1>
+    </div>
+  )
+}
+
+const Content = ({parts}) => {
+    return(
+      <ul>
+      {parts.map(part => <li key={part.id}> {part.name} {part.exercises} </li>)}
+      </ul>
+  )
+}
+
+const Total = ({parts}) => {
+  var total = 0
+  parts.forEach((part) => total = total + part.exercises);
+  console.log(total);
+  return(
+    <div>
+    <b> total number of exercises:  {total} </b>
+    </div>
+  )
+}
+
+export default App
